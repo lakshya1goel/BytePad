09074893b9e87cc73cbc3e0f513b324530d870f3
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-10-28 01:07:07 +0530

    remove unnecessary token verification code & display error in textform field

--- lib/Services/token_generation.dart
+++ lib/Services/token_generation.dart
@@ -1,5 +1,3 @@
-import 'package:bytepad/Services/token_verification.dart';
-import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../Models/generate_token_error.dart';
@@ -21,9 +19,8 @@ Future<String?> loginUser(String email, String password) async {
   if (response.statusCode == 200) {
     Map<String, dynamic> generateResponse = jsonDecode(response.body);
     GenerateTokenResponse generateTokenResponse = GenerateTokenResponse.fromJson(generateResponse);
-    var accessToken = generateTokenResponse.access;
-    // print(jsonDecode(response.body));
-    return verifyToken(accessToken);
+    print(jsonDecode(response.body));
+    return null;
   }
   else {
     Map<String, dynamic> errorResponse = jsonDecode(response.body);
--- lib/Views/Widgets/custom_input_field.dart
+++ lib/Views/Widgets/custom_input_field.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import '../../Contollers/validation.dart';
+import '../../Services/token_generation.dart';
 import '../../theme_data.dart';
 
 class CustomInputField extends StatelessWidget {
   final String labelText;
   final IconData icon;
   final TextEditingController controller;
-  const CustomInputField({ required this.labelText, required this.icon, required this.controller,Key? key,}) : super(key: key);
+  final TextEditingController? emailController;
+  final TextEditingController? passwordController;
+  final GlobalKey<FormState>? formKey;
+  const CustomInputField({ required this.labelText, required this.icon, required this.controller, this.emailController, this.passwordController, this.formKey, Key? key,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,31 +19,52 @@ class CustomInputField extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4.0),
-      child: TextFormField(
-        controller: controller,
-        cursorColor: blueColor,
-        decoration: InputDecoration(
-          hintText: labelText,
-          border: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: blueColor),
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          prefixIcon: Container(
-            margin: EdgeInsets.only(right: size.width*0.08),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                color: blueColor,
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(17.0),
-                child: Icon(icon,
-                  color: Colors.white,
+      child: Form(
+        key: formKey,
+        child: TextFormField(
+          controller: controller,
+          cursorColor: blueColor,
+          validator: (value) {
+            String? emailError = emailController?.text != null
+                ? Validator.isValidEmail(emailController!.text)
+                : null;
+
+            if (emailError != null) {
+              return emailError;
+            }
+
+            String? passwordError = passwordController?.text != null
+                ? Validator.isValidPassword(passwordController!.text)
+                : null;
+
+            if (passwordError != null) {
+              return passwordError;
+            }
+
+          },
+          decoration: InputDecoration(
+            hintText: labelText,
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: blueColor),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            prefixIcon: Container(
+              margin: EdgeInsets.only(right: size.width*0.08),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  color: blueColor,
                 ),
-              )
+                child: Padding(
+                  padding: const EdgeInsets.all(17.0),
+                  child: Icon(icon,
+                    color: Colors.white,
+                  ),
+                )
+            ),
           ),
         ),
       ),
