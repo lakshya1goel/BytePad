4899e5f5c2ad104062abfe7a242ed7cb9a3dd865
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-11-13 18:40:26 +0530

    add changes

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: bgColor,
       ),
-      home: const MyCollections(),
+      home: const SplashScreen(),
       routes: {
         '/Login' : (context) => const LoginPage()
       },
