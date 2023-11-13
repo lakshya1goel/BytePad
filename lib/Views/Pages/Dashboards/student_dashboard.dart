894cad83897a8356ce93b9039dde7ed3143c800a
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-11-13 14:33:50 +0530

    add carouselslider for forums

--- lib/Views/Pages/Dashboards/student_dashboard.dart
+++ lib/Views/Pages/Dashboards/student_dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:bytepad/Utils/Constants/colors.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class StudentDashboard extends StatefulWidget {
@@ -9,6 +10,16 @@ class StudentDashboard extends StatefulWidget {
 }
 
 class _StudentDashboardState extends State<StudentDashboard> {
+
+  List display = [
+    {"heading": 'CSE-1 Digital Forum', "content": 'Could someone please send the notes...'},
+    {"heading": 'CSE-1 Digital Forum', "content": 'Could someone please send the notes...'},
+    {"heading": 'CSE-1 Digital Forum', "content": 'Could someone please send the notes...'},
+  ];
+
+  final CarouselController carouselController = CarouselController();
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
 
@@ -49,6 +60,91 @@ class _StudentDashboardState extends State<StudentDashboard> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(height: size.height*0.05,),
+              Container(
+                height: size.height*0.2,
+                width: double.infinity,
+                child: CarouselSlider(
+                  items: display.map((item) => Card(
+                    child: Container(
+                      width: size.width*0.9,
+                      child: Row(
+                        children: [
+                          Container(
+                            color: blueColor,
+                            height: size.height*0.2,
+                            width: size.width*0.03,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: size.width*0.05, vertical: size.height*0.02),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Color(0x332B27FD),
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(2.0),
+                                    child: Text('New', style: TextStyle(color: blueColor, fontSize: size.width*0.03),),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left : size.width*0.05, right : size.width*0.05, bottom: size.height*0.01),
+                                child: Text(item['heading'],
+                                  style: TextStyle(
+                                      fontSize: size.width*0.05,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ),
+                              // SizedBox(height: size.height*0.05),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: size.width*0.05,),
+                                child: Text(item['content'],
+                                  style: TextStyle(
+                                    fontSize: size.width*0.035,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  )).toList(),
+                  carouselController: carouselController,
+                  options: CarouselOptions(
+                      scrollPhysics: const BouncingScrollPhysics(),
+                      aspectRatio: 0.55,
+                      viewportFraction: 1,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          currentIndex = index;
+                        });
+                      }
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: display.asMap().entries.map((entry) {
+                  return GestureDetector(
+                    onTap: () => carouselController.animateToPage(entry.key),
+                    child: Container(
+                      width: size.width*0.025,
+                      height: size.height*0.012,
+                      margin: const EdgeInsets.symmetric(horizontal: 3),
+                      decoration: BoxDecoration(
+                        color: currentIndex == entry.key? blueColor : Colors.grey,
+                        borderRadius: BorderRadius.circular(size.width*0.025),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
               Padding(
                 padding: EdgeInsets.all(size.width*0.05),
                 child: Text("Quick Links",
