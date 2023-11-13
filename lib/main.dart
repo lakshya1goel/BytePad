9c8350d0d6cf7153737e8c01ca17866edd7287ef
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-11-13 18:13:23 +0530

    add basic structure of my collections screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'Views/Pages/DocumentViewScreens/documents_listing_screen.dart';
 import 'Views/Pages/Dashboards/hod_dashboard.dart';
 import 'Views/Pages/Dashboards/student_dashboard.dart';
+import 'Views/Pages/DocumentViewScreens/papers_collection.dart';
 
 void main() async{
   await dotenv.load(fileName: ".env");
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: bgColor,
       ),
-      home: const SplashScreen(),
+      home: const MyCollections(),
       routes: {
         '/Login' : (context) => const LoginPage()
       },
