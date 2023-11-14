53badae0016117caca2e7f785fbaa3700faa5cb4
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-11-14 21:05:20 +0530

    complete the ui for papers listing page

--- lib/Views/Pages/DocumentViewScreens/documents_listing_screen.dart
+++ lib/Views/Pages/DocumentViewScreens/documents_listing_screen.dart
@@ -71,7 +71,7 @@ class _DocumentListingScreenState extends State<DocumentListingScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: EdgeInsets.symmetric(horizontal: size.width*0.07, vertical: size.height*0.02),
+                padding: EdgeInsets.symmetric(horizontal: size.width*0.07,),
                 child: Text('Past Exams',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
@@ -79,6 +79,48 @@ class _DocumentListingScreenState extends State<DocumentListingScreen> {
                   ),
                 ),
               ),
+              SizedBox(height: size.height*0.02,),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: size.width*0.05),
+                child: Row(
+                  children: [
+                    Container(
+                        width: size.width*0.7,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF979797).withOpacity(0.25),
+                          borderRadius: BorderRadius.circular(25.0),
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.search,
+                                color: Color(0xFF979797)
+                            ),
+                            hintText: 'What are you looking for?',
+                            hintStyle: TextStyle(color: Color(0xFF979797)),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.all(16.0),
+                          ),
+                        ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: size.width*0.02),
+                      child: Container(
+                        width: size.width*0.17,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF979797).withOpacity(0.25),
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: IconButton(
+                            onPressed: () {},
+                            icon: Image.asset("assets/images/SlidersHorizontal.png"),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: size.height*0.02,),
               FutureBuilder<PaperListingModel?>(
                 future: papersFuture,
                 builder: (context, snapshot) {
@@ -103,24 +145,70 @@ class _DocumentListingScreenState extends State<DocumentListingScreen> {
                           Results paper = snapshot.data!.results![index];
                           return GestureDetector(
                             onTap: (){
-
+                              print("hhhhhhhhh");
                             },
-                            child: Card(
-                              child: ListTile(
-                                title: Text(paper.title ?? ''),
-                                subtitle: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(paper.courses.toString() ?? ''),
-                                    Row(
-                                      children: [
-                                        Text(paper.year.toString() ?? ''),
-                                        SizedBox(width: size.width*0.75,),
-                                        Text(paper.semester.toString() ?? ''),
-                                      ],
+                            child: Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Container(
+                                width: size.width*0.95,
+                                height: size.height*0.15,
+                                child: ListTile(
+                                    title: Padding(
+                                      padding: EdgeInsets.only(top: 8.0),
+                                      child: Text(paper.title ?? '',
+                                        style: TextStyle(color: Colors.grey),
+                                      ),
                                     ),
+                                    subtitle: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text(paper.courses.toString() ?? '',
+                                              style: TextStyle(color: Color(0xFF656565), fontWeight: FontWeight.bold),
+                                            ),
+                                            SizedBox(width: size.width*0.28),
+                                            IconButton(
+                                                onPressed: (){
+                                                  print("dddddd");
+                                                },
+                                                icon: Icon(Icons.download,)),
+                                            IconButton(
+                                                onPressed: (){
+                                                  print("fffffff");
+                                                },
+                                                icon: Icon(Icons.create_new_folder,)),
+                                            IconButton(
+                                                onPressed: (){
+                                                  print("sssssss");
+                                                },
+                                                icon: Icon(Icons.share,)),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Padding(
+                                              padding: EdgeInsets.only(right: 8.0),
+                                              child: Text("Sem: ${paper.semester.toString() ?? ''}"),
+                                            ),
+                                            Text("Year: ${paper.year.toString() ?? ''}"),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                ),
+                                decoration: ShapeDecoration(
+                                  color: Colors.white,
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                                  shadows: [
+                                    BoxShadow(
+                                      color: Color(0x3F000000),
+                                      blurRadius: 4,
+                                      offset: Offset(0, 4),
+                                      spreadRadius: 0,
+                                    )
                                   ],
-                                )
+                                ),
                               ),
                             ),
                           );
