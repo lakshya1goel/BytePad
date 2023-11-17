a27503600f0ffd9793fde1895e6310092cdff8f7
lakshya1goel <118365845+lakshya1goel@users.noreply.github.com>
2023-11-17 23:09:36 +0530

    add model for hod & faculty details

---
+++ lib/Services/Details/hod_faculty_details.dart
@@ -0,0 +1,36 @@
+import 'package:bytepad/Models/Details/hod_faculty_details_model.dart';
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+Future<HodFacultyDetailsModel?> getHodFacultyDetails(String? authToken) async {
+  print('hhhhhhhhhhh');
+  try {
+    final String baseUrl = dotenv.get('BaseUrl');
+    var url = Uri.parse('$baseUrl/details/user/');
+
+    final response = await http.get(
+      url,
+      headers: {
+        'accept': 'application/json',
+        'Authorization': 'Bearer $authToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      // Successful request, parse the response data into StudentDetailsModel
+      final responseData = json.decode(response.body);
+      print(responseData);
+      return HodFacultyDetailsModel.fromJson(responseData);
+    } else {
+      // Request failed, handle error accordingly
+      print('Request failed with status: ${response.statusCode}');
+      print('Response body: ${response.body}');
+      return null;
+    }
+  } catch (e) {
+    // Handle any exceptions that occur during the HTTP request
+    print('Exception during request: $e');
+    return null;
+  }
+}
